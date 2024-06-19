437bb1e39da95a2addb1ffcc43ef59595a906771
assylkhan <assylkhan.rakhatov@mail.ru>
2024-06-20 01:08:08 +0500

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 import useGame from "../hooks/useGame";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,7 +15,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
